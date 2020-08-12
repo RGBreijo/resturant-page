00704fd623489e628187f3714ee5f8d84b7d3c52
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-08-12 10:51:45 -0400

    changes to comply with the seperation of navbar into a module

--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 
-import {navigationBar, pageHeader, mainPageContent, emailSection} from './mainPage.js';
-import {menuContent} from './menu.js';
+import {renderHomePage} from './homePage.js';
+import {renderNavigationbar} from './navigationBar'
 
-menuContent(); 
-
-let indexContainer = document.querySelector(".content");
-
-// NAVIGATION BAR 
-indexContainer.appendChild(navigationBar());
-document.querySelector("#mobileHamburger").addEventListener('click', OpenMobileNavBarAnimation);
-
-
-// INDEX MAIN IMAGE
-indexContainer.appendChild(pageHeader("pizzaPlanetPhotos/outsidePizzaPlanet.jpg"));
-
-
-// MAIN CONTENT 
-
-indexContainer.appendChild(mainPageContent("pizzaPlanetPhotos/pizzaPlanetGames.jpg"));
-
-
-// EMAIL SECTION 
-indexContainer.appendChild(emailSection());
+renderHomePage(); 
+renderNavigationbar();
 
 
 
@@ -31,19 +13,8 @@ indexContainer.appendChild(emailSection());
 
 
 
-function OpenMobileNavBarAnimation()
-{
-    let expandMobileNavBar = document.querySelector(".mobileNavContainer");
-    expandMobileNavBar.style.width = '200px'; 
-
-    
-    document.querySelector("#closeMobileNav").addEventListener('click', closeMobileNavBarAnimiation);
-}
 
 
-function closeMobileNavBarAnimiation()
-{
-    let closeMobileNavBar = document.querySelector(".mobileNavContainer");
-    closeMobileNavBar.style.width = '0'; 
-}
+
+
 
